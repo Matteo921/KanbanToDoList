835d99d2bdc3bc405a8022bf2495dd52c2692bdf
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2019-10-19 15:55:15 +0200

    Add board

---
+++ js/app.js
@@ -0,0 +1,44 @@
+'use strict'
+
+// var prefix = "https://cors-anywhere.herokuapp.com/";
+// var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+// var myHeaders = {
+//   'X-Client-Id': '4000',
+//   'X-Auth-Token': '4fc5defc6a0b7c4692c4bdfbdd2c5a60'
+// };
+
+fetch(prefix + baseUrl + '/board', { headers: myHeaders })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(resp) {
+        setupColumns(resp.columns);
+    });
+
+//Create columns
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+//Create cards
+function setupCards(col, cards) {
+    cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+    col.addCard(cardObj);
+    });
+}
+
+//Generate template
+function generateTemplate(name, data, basicElement) {
+    var template = document.getElementById(name).innerHTML;
+    var element = document.createElement(basicElement || 'div');
+  
+    Mustache.parse(template);
+    element.innerHTML = Mustache.render(template, data);
+  
+    return element;
+};
